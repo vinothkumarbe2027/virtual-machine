313f72cf6b52ae13d1663bdd1c7471fd74f8dbb8
vinothkumarbe2027 <vinothkumarbe2027@gmail.com>
2025-12-10 06:42:41 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,75 @@
+provider "azurerm" {
+  features {}
+}
+
+Resource Group
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-winvm-demo"
+  location = "East US"
+}
+
+️Virtual Network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-win-demo"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+️Subnet
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet-winvm"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+️Public IP
+resource "azurerm_public_ip" "pip" {
+  name                = "pip-winvm"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+Network Interface
+resource "azurerm_network_interface" "nic" {
+  name                = "nic-winvm"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "ipconfig1"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pip.id
+  }
+}
+
+Windows Virtual Machine
+resource "azurerm_windows_virtual_machine" "vm" {
+  name                = "winvm-demo01"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_B2ms"
+
+  admin_username = "adminuser"
+  admin_password = "Password@1234"  # change this
+
+  network_interface_ids = [
+    azurerm_network_interface.nic.id
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2022-datacenter"
+    version   = "latest"
+  }
+}
