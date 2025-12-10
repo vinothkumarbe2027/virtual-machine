ad0d049398183ae44c913f75008c1028990e9fea
vinothkumarbe2027 <vinothkumarbe2027@gmail.com>
2025-12-10 06:44:07 +0530

    Create outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,11 @@
+output "public_ip" {
+  value = azurerm_public_ip.pip.ip_address
+}
+
+output "private_ip" {
+  value = azurerm_network_interface.nic.private_ip_address
+}
+
+output "vm_id" {
+  value = azurerm_windows_virtual_machine.vm.id
+}
