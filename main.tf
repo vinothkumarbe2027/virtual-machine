268d23b0f49cfcf98bb0639036b3efe2ae0f3b1c
vinothkumarbe2027 <vinothkumarbe2027@gmail.com>
2025-12-10 07:39:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,47 +1,43 @@
-terraform {
-  backend "azurerm" {}
-}
-
 provider "azurerm" {
   features {}
 }
 
 # Resource Group
 resource "azurerm_resource_group" "rg" {
-  name     = "rg-winvm-demo"
-  location = "East US"
+  name     = var.rg_name
+  location = var.location
 }
 
 # Virtual Network
 resource "azurerm_virtual_network" "vnet" {
-  name                = "vnet-win-demo"
-  address_space       = ["10.0.0.0/16"]
+  name                = var.vnet_name
+  address_space       = var.address_space
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
 
 # Subnet
 resource "azurerm_subnet" "subnet" {
-  name                 = "subnet-winvm"
+  name                 = var.subnet_name
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.1.0/24"]
+  address_prefixes     = var.subnet_prefix
 }
 
 # Public IP
 resource "azurerm_public_ip" "pip" {
-  name                = "pip-winvm"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  name                = "${var.vm_name}-pip"
+  location            = var.location
+  resource_group_name = var.rg_name
   allocation_method   = "Static"
   sku                 = "Standard"
 }
 
 # Network Interface
 resource "azurerm_network_interface" "nic" {
-  name                = "nic-winvm"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  name                = "${var.vm_name}-nic"
+  location            = var.location
+  resource_group_name = var.rg_name
 
   ip_configuration {
     name                          = "ipconfig1"
@@ -51,15 +47,15 @@ resource "azurerm_network_interface" "nic" {
   }
 }
 
-# Windows Virtual Machine
+# Windows VM
 resource "azurerm_windows_virtual_machine" "vm" {
-  name                = "winvm-demo01"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  size                = "Standard_B2ms"
+  name                = var.vm_name
+  resource_group_name = var.rg_name
+  location            = var.location
+  size                = var.vm_size
 
-  admin_username = "adminuser"
-  admin_password = "Password@1234"  # change this
+  admin_username = var.admin_username
+  admin_password = var.admin_password
 
   network_interface_ids = [
     azurerm_network_interface.nic.id
@@ -77,4 +73,5 @@ resource "azurerm_windows_virtual_machine" "vm" {
     version   = "latest"
   }
 }
+
 
