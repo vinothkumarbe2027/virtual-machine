f1265d78c724e7cd209bb61b63c89b0d5802f461
vinothkumarbe2027 <vinothkumarbe2027@gmail.com>
2025-12-10 09:40:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,42 +2,63 @@ provider "azurerm" {
   features {}
 }
 
-# Resource Group
+# ----------------------
+# RESOURCE GROUP
+# ----------------------
 resource "azurerm_resource_group" "rg" {
   name     = var.rg_name
   location = var.location
 }
 
-# Virtual Network
+# ----------------------
+# VIRTUAL NETWORK
+# ----------------------
 resource "azurerm_virtual_network" "vnet" {
+  depends_on = [azurerm_resource_group.rg]
+
   name                = var.vnet_name
   address_space       = var.address_space
-  location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
+  location            = var.location
 }
 
-# Subnet
+# ----------------------
+# SUBNET
+# ----------------------
 resource "azurerm_subnet" "subnet" {
+  depends_on = [azurerm_virtual_network.vnet]
+
   name                 = var.subnet_name
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = var.subnet_prefix
 }
 
-# Public IP
+# ----------------------
+# PUBLIC IP
+# ----------------------
 resource "azurerm_public_ip" "pip" {
+  depends_on = [azurerm_resource_group.rg]
+
   name                = "${var.vm_name}-pip"
-  location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
+  location            = var.location
   allocation_method   = "Static"
   sku                 = "Standard"
 }
 
-# Network Interface
+# ----------------------
+# NETWORK INTERFACE
+# ----------------------
 resource "azurerm_network_interface" "nic" {
+  depends_on = [
+    azurerm_public_ip.pip,
+    azurerm_subnet.subnet
+  ]
+
   name                = "${var.vm_name}-nic"
-  location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
+  location            = var.location
 
   ip_configuration {
     name                          = "ipconfig1"
@@ -47,19 +68,21 @@ resource "azurerm_network_interface" "nic" {
   }
 }
 
-# Windows VM
+# ----------------------
+# WINDOWS VM
+# ----------------------
 resource "azurerm_windows_virtual_machine" "vm" {
+  depends_on = [azurerm_network_interface.nic]
+
   name                = var.vm_name
   resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
+  location            = var.location
   size                = var.vm_size
 
   admin_username = var.admin_username
   admin_password = var.admin_password
 
-  network_interface_ids = [
-    azurerm_network_interface.nic.id
-  ]
+  network_interface_ids = [azurerm_network_interface.nic.id]
 
   os_disk {
     caching              = "ReadWrite"
@@ -73,6 +96,7 @@ resource "azurerm_windows_virtual_machine" "vm" {
     version   = "latest"
   }
 }
+
 
 
 
