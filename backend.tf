0ba521006df9f5e34fd1313f864a484448ceab45
vinothkumarbe2027 <vinothkumarbe2027@gmail.com>
2025-12-10 07:41:41 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,9 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "rg-tfstate"
-    storage_account_name = "vmcontainer"
+    storage_account_name = "sttfstate007"
     container_name       = "vmcontainer"
     key                  = "windowsvm.tfstate"
   }
 }
+
